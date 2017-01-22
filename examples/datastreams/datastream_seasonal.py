d71148bc025d1337e25c1e40ca58d5ba58ab384f
cbode <collin@berkeley.edu>
2017-01-22 15:16:25 -0800

    added gitignore

--- examples/datastreams/datastream_seasonal.py
+++ examples/datastreams/datastream_seasonal.py
@@ -21,6 +21,7 @@
 import mysql.connector
 import datetime as dt
 import pandas as pd
+import re
 
 def odm_connect(pwfilepath,boo_dev=False):
     # NOTE: password file should NEVER be uploaded to github!
@@ -45,19 +46,18 @@ dslist = {
     3106:'Soil Moisture VWC Blue Oak Avg',
     3069:'Wind Speed m s Blue Oak Avg',
     3080:'Relative Humidity Per Blue Oak Avg',
-    3081:'Barometric Pressure mbar Blue Oak Avg'
+    3081:'Barometric Pressure mbar Blue Oak Avg',
+    #3061:'Total Solar Radiation W m2 Blue Oak Avg',
+    3082:'Precipitation mm Blue Oak',
+    3355:'Air Temp Deg C 2 m Angelo Avg',
+    3373:'Soil Temp Deg C 20 in Angelo Avg',
+    3374:'Soil Moisture VWC Angelo Avg',
+    3336:'Wind Speed m s Angelo Avg',
+    3348:'Relative Humidity Per Angelo Avg',
+    3349:'Barometric Pressure mbar Angelo Avg',
+    #3329:'Total Solar Radiation W m2 Angelo Avg',
+    3350:'Precipitation mm Angelo'
 }
-
-# Connect to Sensor Database and get oldest year for DSID
-conn = odm_connect('odm.pw',boo_dev=True)
-sql_oldest_year = 'SELECT min(year(LocalDateTime)) '+\
-' FROM odm.datavalues_UCNRS '+\
-' WHERE DatastreamID = 3077'
-cursor = conn.cursor()
-cursor.execute(sql_oldest_year)
-year_oldest = cursor.fetchall()[0][0]
-print('Oldest Year: ',year_oldest)
-conn.close()
 
 # Get current year
 now = dt.datetime.now()
@@ -65,7 +65,20 @@ year_current = now.year
 
 for dsid,dsname in dslist.items():
     print(dsid,dsname)
-    
+    if(re.match('Precipitation',dsname)):
+        print('TRUE! I found Precipitation:',dsid,dsname)
+
+    # Connect to Sensor Database and get oldest year for DSID
+    conn = odm_connect('odm.pw',boo_dev=True)
+    sql_oldest_year = 'SELECT min(year(LocalDateTime)) '+\
+    ' FROM odm.datavalues_UCNRS '+\
+    ' WHERE DatastreamID = 3077'
+    cursor = conn.cursor()
+    cursor.execute(sql_oldest_year)
+    year_oldest = cursor.fetchall()[0][0]
+    print('Oldest Year: ',year_oldest)
+    conn.close()
+
     for year in range(year_oldest+1,year_current):
         print('Year: ',year)
         
@@ -74,7 +87,7 @@ for dsid,dsname in dslist.items():
         'count(*) as count_hour, '+\
         'min(DataValue) as min, '+\
         'avg(DataValue) as avg, '+\
-        'max(DataValue) as max '+\
+        'max(DataValue) as max, '+\
         'sum(DataValue) as sum '+\
         'FROM odm.datavalues_UCNRS '+\
         "WHERE datastreamid = "+str(dsid)+" and LocalDateTime < '"+str(year)+"-01-01 00:00:00' "+\
@@ -84,17 +97,16 @@ for dsid,dsname in dslist.items():
         conn = odm_connect('odm.pw',boo_dev=True)
         cursor = conn.cursor()
         cursor.execute(sql_daily)
-        #result = cursor.fetchall()
-        #print(result)
-        #dfday = pd.read_sql_query(sql_daily,conn,index_col='ldate',parse_dates=True)
     
-        #sql_monthly = 'CREATE TEMPORARY TABLE ztemp_month '+\
         sql_monthly = 'SELECT month(ldate) as month, '+\
         'count(*) as count_month, '+\
         'sum(count_hour) as count_hour, '+\
         'avg(min) as min_avg, '+\
         'avg(avg) as avg_avg, '+\
-        'avg(max) as max_avg  '+\
+        'avg(max) as max_avg, '+\
+        'min(sum) as min_sum, '+\
+        'avg(sum) as avg_sum, '+\
+        'max(sum) as max_sum '+\
         'FROM ztemp_date GROUP BY month(ldate)'
         #print(sql_monthly)
         df = pd.read_sql_query(sql_monthly,con=conn)
@@ -108,19 +120,26 @@ for dsid,dsname in dslist.items():
             mmin = df['min_avg'][i]
             mavg = df['avg_avg'][i]
             mmax = df['max_avg'][i]
+            mminsum = df['min_sum'][i]
+            mavgsum = df['avg_sum'][i]
+            mmaxsum = df['max_sum'][i]
             
             # Insert monthly values into the datavalues_seasonal table
             SeasonDateTime = str(year)+'-'+str(month).zfill(2)+'-01 00:00:00'
-            print(SeasonDateTime,dsidmin,mmin,dsidmax,mmax,' INSERTING')
+            print(SeasonDateTime,dsid,dsname,' INSERTING')
             # DataValue,ValueAccuracy,SeasonDateTime,UTCOffset,QualifierID,DerivedFromID,QualityControlLevelCode,DatastreamID  
             sql_insert = 'INSERT into datavalues_seasonal (DataValue,LocalDateTime,'+\
             'UTCOffset,QualifierID,DerivedFromID,QualityControlLevelCode,DatastreamID) '+\
             'Values (%s,%s,%s,%s,%s,%s,%s)'
-            #cursor.execute(sql_insert,(float(mmin),SeasonDateTime,-8,2,dsid,2,dsidmin))
-            #cursor.execute(sql_insert,(float(mavg),SeasonDateTime,-8,2,dsid,2,dsidavg))
-            #cursor.execute(sql_insert,(float(mmax),SeasonDateTime,-8,2,dsid,2,dsidmax))
+            if(re.match('Precipitation',dsname)):
+                cursor.execute(sql_insert,(float(mminsum),SeasonDateTime,-8,2,dsid,2,dsidmin))
+                cursor.execute(sql_insert,(float(mavgsum),SeasonDateTime,-8,2,dsid,2,dsidavg))
+                cursor.execute(sql_insert,(float(mmaxsum),SeasonDateTime,-8,2,dsid,2,dsidmax))
+            else:   
+                cursor.execute(sql_insert,(float(mmin),SeasonDateTime,-8,2,dsid,2,dsidmin))
+                cursor.execute(sql_insert,(float(mavg),SeasonDateTime,-8,2,dsid,2,dsidavg))
+                cursor.execute(sql_insert,(float(mmax),SeasonDateTime,-8,2,dsid,2,dsidmax))
             
         cursor.close()
         conn.close()
-    
 print('DONE!')

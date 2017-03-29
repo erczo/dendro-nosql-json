a3897a3db8d77db27bf4f913fda463b01d621334
cbode <collin@berkeley.edu>
2017-03-29 11:38:10 -0700

    mongo loader
    Stations loader functional, but has bug that causes 500 error in a
    non-reproducable fashion.

--- migration/mongo_loader.py
+++ migration/mongo_loader.py
@@ -12,14 +12,14 @@
 # Requires: 
 #   migration_stations.py must be run first
 #   migration_datastreams_dashboard.py must be run first
-#   Assumes station, datastream directories
+#   Assumes station, datastream directories                      
+# http://www.jaimegil.me/2012/12/26/a-python-restful-api-consumer.html                      
 ########################################
+import os
 import json
 import requests
-import datetime as dt
-import pandas as pd
-import os
-import sys
+import glob
+import time
 
 # ARGS
 # What are we loading?
@@ -28,13 +28,12 @@ target = 'stations'
 #target = 'organizations'
 
 # Set paths
-path = os.path.dirname(__file__)+os.sep+target+os.sep
-tpath = path+'ztemp'+os.sep
+path = os.path.dirname(__file__)+'/'+target+'/'
+tpath = path+'/'+'ztemp'+'/'
 if(os.path.exists(tpath) == False):
     os.mkdir(tpath)
-                      
-# http://www.jaimegil.me/2012/12/26/a-python-restful-api-consumer.html                      
-# Set URL
+
+# Set URL to Station and pull station JSON list
 url = 'http://128.32.109.75/api/v1/'+target
 header = {"Content-Type":"application/json"}
 r = requests.get(url,headers=header)
@@ -43,6 +42,7 @@ rjson = r.json()
 total = rjson['total']
 print('Entries found: ',total)
 
+# For each station, dump into a temp directory the JSON content
 rdata = rjson['data']
 for entry in rdata:
     print(entry['name'],entry['_id'])
@@ -52,13 +52,37 @@ for entry in rdata:
     with open(ffile, 'w') as f:
          json.dump(entry, f, indent=2,sort_keys=True)
 
-for file in os.listdir(path): 
-    print(file)
-    with open(path+file) as json_data:
+# Go through all the station JSON exported by migration_stations.py
+# Check for a name match with existing stations, if it exists, delete it from Mongo
+# Then post the new JSON into Mongo
+print()
+print('Loading JSON files now')
+for filepath in glob.glob(path+"*.json"): 
+    print(filepath)
+    with open(filepath) as json_data:
         d = json.load(json_data)
+        name = d['name']
+        print(name)
         #print(json.dumps(d,indent=2,sort_keys=True))
+        
+        # Check for a match with existing station entries
         for entry in rdata:
-            if(d['name'] == entry['name']):
-                print('MATCH! '+d['name'],entry['name'],entry['_id'])
+            if(name == entry['name']):
+                print('MATCH! '+name,entry['name'],entry['_id'])
+                r2 = requests.delete(url+'/'+entry['_id'])
+                assert r2.status_code == 200
+                print('DELETED '+name+' in MongoDB')
+        # Debug wait to deal with 500 internal error        
+        time.sleep(2)
+        print('slept 2 sec')
+        
+        # Insert new station JSON
+        r3 = requests.post(url,headers=header,data=json.dumps(d))
+        assert r3.status_code == 201        
+        print(name+' inserted into MongoDB')
+        
+        time.sleep(2)
+        print('slept 2 sec')
+
 
 print('DONE!')                 
